158fa2107611cd3110196e99be5fc486467433f1
jayden220 <jaydenvanrooyen87@gmail.com>
2024-09-16 16:12:18 +0200

    created

---
+++ src/components/NavbarComp.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="sticky-top">
+        <nav class="navbar  bg-dark">
+    <div class="container-fluid">
+      <a class="brand">J V R</a>
+       <nav> 
+        <a href="#">Home</a> |
+        <a href="#about">About</a> |
+        <a href="#education">Education</a> |
+        <a href="#skills">Skills</a> |
+        <a href="#projects">Projects</a> |
+        <a href="#test">Testimonials</a> |
+        <a href="#contact">Contact</a> 
+       </nav>
+    </div>
+        </nav>
+    </div> 
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,44 @@
 <template>
-  <div class="home">
-  </div>
+
+   <div>
+    <navbar-comp/>
+    <about-comp/>
+    <skills-comp/>
+    <education-comp/>
+    <projects-comp/>
+    <footer-comp/>
+
+   </div>
+
+
+ 
 </template>
 
+
+
+
 <script>
+import AboutComp from '@/components/AboutComp.vue';
+import EducationComp from '@/components/EducationComp.vue';
+import NavbarComp from '@/components/NavbarComp.vue';
+import ProjectsComp from '@/components/ProjectsComp.vue';
+import SkillsComp from '@/components/SkillsComp.vue';
+import FooterComp from '@/components/FooterComp.vue';
+
+
 
 
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  components:{
+    NavbarComp,
+    AboutComp,
+    SkillsComp,
+    EducationComp,
+    ProjectsComp,
+    FooterComp
+
+  }
 
 }
 </script>
